92677bd5792935f1f3f9a8d122b5f74caa7c1c2d
MATY Codes <122626631+TUYISHIMEMaximillien75@users.noreply.github.com>
2025-09-19 20:10:54 +0200

    Update Globale.gd

--- Scenes/Globale.gd
+++ Scenes/Globale.gd
@@ -1,8 +1,5 @@
 extends Node
-var testNum = 3
-#var SCREEN_WIDTH
-#const SCREEN_WIDTH = 1152
-#const SCREEN_HEIGHT = 648
-var timer1 = 5
-var timer2 = 7
-var idle_time = 3.0
+
+var score: int = 0
+@export var speed: float = 200
+var idle_timer = 0.0
